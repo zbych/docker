05d5b0a5cf4a012f4a6e654f74c154b324b38402
Zbigniew Labacz <zbigniew.labacz@hatimeria.pl>
2017-08-04 16:13:32 +0200

    Fix in php5

--- php5-m1/.docker/run.sh
+++ php5-m1/.docker/run.sh
@@ -7,7 +7,7 @@ dockerize \
     -template=/var/docker/php5/cli/php.ini:/etc/php5/cli/php.ini \
     -template=/var/docker/php5/fpm/php.ini:/etc/php5/fpm/php.ini \
     -template=/var/docker/php5/fpm/www.conf:/etc/php5/fpm/pool.d/www.conf \
-    -template=/var/docker/php5/xdebug.conf:/etc/php5/mods-available/xdebug.conf \
+    -template=/var/docker/php5/xdebug.ini:/etc/php5/mods-available/xdebug.ini \
     /bin/true
 
 # Clean logs
