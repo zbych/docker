07d25930cb866a61d683e55d5803b744837e68e9
zbych <zbigniew.labacz@gmail.com>
2017-08-05 21:03:35 +0200

    Fix in PHP7

--- php7-m2/.docker/run.sh
+++ php7-m2/.docker/run.sh
@@ -7,7 +7,7 @@ dockerize \
     -template=/var/docker/php7/cli/php.ini:/etc/php/7.0/cli/php.ini \
     -template=/var/docker/php7/fpm/php.ini:/etc/php/7.0/fpm/php.ini \
     -template=/var/docker/php7/fpm/www.conf:/etc/php/7.0/fpm/pool.d/www.conf \
-    -template=/var/docker/php7:/etc/php/7.0/mods-available/xdebug.ini
+    -template=/var/docker/php7/xdebug.ini:/etc/php/7.0/mods-available/xdebug.ini
 
 # Clean logs
 echo "" > /var/log/nginx/access.log
