6b5a3a58f82fd6c27761f0895b98d6384fe092e6
Zbigniew Labacz <zbigniew.labacz@hatimeria.pl>
2018-10-24 09:56:55 +0200

    Auto configure M1

--- php5-m1/.docker/run.sh
+++ php5-m1/.docker/run.sh
@@ -8,6 +8,7 @@ dockerize \
     -template=/var/docker/php5/fpm/php.ini:/etc/php5/fpm/php.ini \
     -template=/var/docker/php5/fpm/www.conf:/etc/php5/fpm/pool.d/www.conf \
     -template=/var/docker/php5/xdebug.ini:/etc/php5/mods-available/xdebug.ini \
+    -template=/var/docker/etc/local.xml:/var/www/current/app/etc/local.xml \
     /bin/true
 
 # Clean logs
