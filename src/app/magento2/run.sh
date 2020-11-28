760de641c4b018f0fffd9b976989e387db08367f
Zbigniew Labacz <zbigniew.labacz@hatimeria.pl>
2020-11-28 13:41:43 +0100

    SSH through the socket, update Magento2 Auth

--- src/app/magento2/run.sh
+++ src/app/magento2/run.sh
@@ -15,6 +15,13 @@ if [[ ! -f %APP_PATH%/nginx.conf ]]; then
     chown www-data:www-data %APP_PATH%/nginx.conf
 fi
 
+echo "Configuring Composer"
+if [[ -f %APP_PATH%/auth.json.sample ]]; then
+  cp %APP_PATH%/auth.json.sample %APP_PATH%/auth.json
+  sed -i -e "s/<public-key>/$MAGENTO_AUTH_USERNAME/" %APP_PATH%/auth.json
+  sed -i -e "s/<private-key>/$MAGENTO_AUTH_PASSWORD/" %APP_PATH%/auth.json
+fi
+
 echo "Setting up cron..."
 if [[ ! -f /var/docker/.built ]] ; then
     echo '* * * * * www-data cd %APP_PATH% && /usr/bin/php bin/magento cron:run >> %APP_PATH%/var/log/cron.log 2>&1' >> /etc/crontab
